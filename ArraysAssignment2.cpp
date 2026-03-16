1ca2ca5ee68aa756cf6d0f6f8d1e7ec00bf738e4
Tran, Wilson <wdtran2018@gmail.com>
2026-03-16 03:00:48 -0700

    Initialized countPerfect() function and added input loop to main()

--- ArraysAssignment2.cpp
+++ ArraysAssignment2.cpp
@@ -11,7 +11,15 @@ int main() {
     int input;
 
     cout << "Enter up to 20 scores (0-100). Enter -1 to stop:\n";
+    cin >> input;
 
+    while(input != -1)
+    {
+        scores[count++] = input;
+
+        cout << "Enter up to 20 scores (0-100). Enter -1 to stop:\n";
+        cin >> input;
+    }
     // Store scores and keep track of how many are entered
     // Report perfect scores using the helper function
   // COMPLETE THE FUNCTION BELOW
@@ -24,8 +32,15 @@ int main() {
 }
 
 // Value-returning function to count scores of 100
-int countPerfect(const int scores[], int size) {
+int countPerfect(const int scores[], int size) 
+{
     int totalPerfect = 0;
     //COMPLETE THE CODE 
+    for(int i = 0; i < size; i++)
+    {
+        if(scores[i] == 100)
+            totalPerfect++;
+    }
+
     return totalPerfect;
 }
